177a388e7cc184e150a1f050700b0d0a9f5902f2
Parnika Mathur <142107096+parnikamathur@users.noreply.github.com>
2024-06-11 08:07:21 +0530

    Delete mysite directory

--- mysite/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import re_path as url, include
-
-urlpatterns = [
-    url(r'^admin/$', admin.site.urls),
-    url(r'^',include('homepage.urls')),
-    
-]
